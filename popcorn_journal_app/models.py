302fda0cdc57f06dc99522e4b273dcbbd161c7a7
jennifernhle <jennifer.nhn.le@gmail.com>
2026-05-08 22:24:25 +0800

    feat: Finalise database models and implement robust seeding

--- popcorn_journal_app/models.py
+++ popcorn_journal_app/models.py
@@ -2,11 +2,7 @@ from popcorn_journal_app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# ** remove comments later: **
-# each class is a table
-# __repr__ is object's string representation - for debugging purposes
-
-# Association table for followers (many-to-many relationship)
+# Association tables
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
@@ -17,35 +13,38 @@ watchlist_items = db.Table('watchlist_items',
     db.Column('movie_id', db.Integer, db.ForeignKey('movie.id', ondelete='CASCADE'), primary_key=True)
 )
 
-# Association table for lists of movies (many-to-many relationship)
 list_movies = db.Table('list_movies',
     db.Column('list_id', db.Integer, db.ForeignKey('list.id'), primary_key=True),
     db.Column('movie_id', db.Integer, db.ForeignKey('movie.id'), primary_key=True)
 )
 
-# Association table for review likes
 review_likes = db.Table('review_likes',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('review_id', db.Integer, db.ForeignKey('review.id'), primary_key=True)
 )
 
+# Models
 class User(db.Model, UserMixin):
-    # USER INFO
+    # User information
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True, nullable=True) # nullable now only because error occurring in registration form, there is no username being passed to the User model when creating a new user, will need to fix this in the registration form and then can set nullable=False
+    username = db.Column(db.String(64), index=True, unique=True, nullable=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    # Profile details
     first_name = db.Column(db.String(64), nullable=True)
     last_name = db.Column(db.String(64), nullable=True)
-    dob = db.Column(db.Date, nullable=True)
-    user_img = db.Column(db.String(256), nullable=True) # URL or file path to user's profile image
     profile_pic = db.Column(db.String(120), default='user.png')
     bio = db.Column(db.String(256), nullable=True)
+
+    # Relationships
     watchlist = db.relationship('Movie', secondary=watchlist_items, backref=db.backref('interested_users', lazy='dynamic'))
     reviews = db.relationship('Review', backref='author', lazy='dynamic', cascade='all, delete-orphan')
     liked_reviews = db.relationship('Review', secondary=review_likes, backref=db.backref('likes', lazy='dynamic'), lazy='dynamic')
     lists = db.relationship('List', backref='owner', lazy='dynamic', cascade='all, delete-orphan')
+    following = db.relationship('User', secondary=followers, primaryjoin=(followers.c.follower_id == id), secondaryjoin=(followers.c.followed_id == id), backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
+    # Password management
     def set_password(self, password):
         self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
 
@@ -54,39 +53,22 @@ class User(db.Model, UserMixin):
             return False
         return check_password_hash(self.password_hash, password)
 
-    # Relationships for followers
-    following = db.relationship('User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-
-    # STATISTICS
-    # Note: Will need to determine the best way to calculate these counts,
-    # such as http GET actual values from database and then get length of each list.
-    # follower_count = db.Column(db.Integer, default=0) # number of followers for the user
-    # review_count = db.Column(db.Integer, default=0) # number of reviews written by the user
-    # list_count = db.Column(db.Integer, default=0) # number of lists created by the user
-    # logged_movie_count = db.Column(db.Integer, default=0) # number of movies watched by the user AGAIN, will need to change this so it is calculated from the length of the list in the database.
-
-    @property
-    def watchlist_count(self):
-        return len(self.watchlist)
-
+    # Social methods
     def follow(self, user):
-        """Follow a user"""
         if not self.is_following(user):
             self.following.append(user)
-            user.follower_count = len(user.followers.all())
 
     def unfollow(self, user):
-        """Unfollow a user"""
         if self.is_following(user):
             self.following.remove(user)
-            user.follower_count = len(user.followers.all())
 
     def is_following(self, user):
-        """Check if current user is following another user"""
         return self.following.filter(followers.c.followed_id == user.id).count() > 0
+
+    # Count of reviews written by user
+    @property
+    def logged_movie_count(self):
+        return self.reviews.count()
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -98,41 +80,48 @@ class Movie(db.Model):
     release_year = db.Column(db.Integer)
     genre = db.Column(db.String(64))
     synopsis = db.Column(db.Text, nullable=True)
-    movie_img = db.Column(db.String(256), nullable=True) # URL or file path to movie cover image
+    movie_img = db.Column(db.String(256), nullable=True)
+    tmdb_id = db.Column(db.Integer, unique=True, nullable=True)
+
+    #Relationship with creator (who added it) and reviews
     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    reviews = db.relationship('Review', backref='movie', lazy=True)
+    reviews = db.relationship('Review', backref='movie', lazy='dynamic', cascade='all, delete-orphan')
+
+    # Average ratings based on overall user reviews
     def get_average_rating(self):
-        if not self.reviews:
+        reviews_list = self.reviews.all()
+        if not reviews_list:
             return 0
-        total = sum(review.rating for review in self.reviews)
-        return total / len(self.reviews)
+        total = sum(review.rating for review in reviews_list)
+        return total / len(reviews_list)
 
     def __repr__(self):
         return f'<Movie {self.title}>'
 
+# Individual user reviews and ratings for movies
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=True) 
-    series_id = db.Column(db.Integer, db.ForeignKey('series.id'), nullable=True)
     
     rating = db.Column(db.Integer) 
     content = db.Column(db.Text)
     date_posted = db.Column(db.DateTime, default=db.func.now())
-    like_count = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return f'<Review {self.id} by {self.user_id}>'
-    
+
+# Collections/lists of movies (public/private)
 class List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # owner of the list
-    name = db.Column(db.String(128)) # name of the list
-    description = db.Column(db.Text) # description of the list
-    public_status = db.Column(db.Boolean, default=False) # public or private list, default = private
-    follower_count = db.Column(db.Integer, default=0) # number of followers for the list
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(128))
+    description = db.Column(db.Text)
+    public_status = db.Column(db.Boolean, default=False)
+
     movies = db.relationship('Movie', secondary='list_movies', backref='contained_in_lists')
 
+    #Count of movies currently in list
     @property
     def movie_count(self):
         return len(self.movies)
